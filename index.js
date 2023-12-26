277b2499ad78d70c35a202010ffa256ab084aabf
IRENE LOPES <113055177+Irene-Lopes@users.noreply.github.com>
2023-12-26 11:10:04 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var elementosduvida = document.querySelectorAll('.duvida')
+
+elementosduvida.forEach(function(duvida) {
+duvida.addEventListener('click', function() {
+    duvida.classList.toggle('ativa')
+  })
+})
+ 
+
+
